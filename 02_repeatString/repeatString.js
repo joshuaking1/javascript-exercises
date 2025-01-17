50ea411cffdbc38255db5d9a62185e335542f95c
joshuaking1 <segujoshua30@gmail.com>
2025-01-17 00:14:54 +0000

    done with this one

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = "";
+    for(i = 0; i < num; i++){
+        result += string
+    };
+
+    return result;
 
 };
 
+console.log('hey', 3)
 // Do not edit below this line
 module.exports = repeatString;
